2b99bad818db91b02ad6b63d7c851c0140ba0b93
Lazar <Lazar@LAZAR>
2025-09-10 16:18:13 +0200

    Dodavanje ISensorService i ServiceResponse klase u Common

---
+++ OfficeSensor/Common/ISensorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface ISensorService
+    {
+        [OperationContract]
+        ServiceResponse StartSession(SessionMetadata metadata);
+
+        [OperationContract]
+        ServiceResponse PushSample(SensorSample sample);
+
+        [OperationContract]
+        ServiceResponse EndSession();
+    }
+}
---
+++ OfficeSensor/Common/ServiceResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class ServiceResponse
+    {
+        [DataMember]
+        public ResponseType Type { get; set; }
+
+        [DataMember]
+        public ResponseStatus Status { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+    }
+
+    [DataContract]
+    public enum ResponseType
+    {
+        [EnumMember]
+        ACK,
+        [EnumMember]
+        NACK
+    }
+
+    [DataContract]
+    public enum ResponseStatus
+    {
+        [EnumMember]
+        IN_PROGRESS,
+        [EnumMember]
+        COMPLETED
+    }
+}
