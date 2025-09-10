7e19c8629f09cf2c58ba37968ed35c97f3f37016
stefanaleksic39 <stefanaleksic555@gmail.com>
2025-09-10 14:26:15 +0100

    Dodavanje ClassLibrary i Server projekta

--- OfficeSensor/Common/Class1.cs
+++ OfficeSensor/Common/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OfficeSensor
+namespace Common
 {
-    public class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
