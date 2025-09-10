1a6da3afbfa45bf9aa3fb31d6b585bd2d5e34793
stefanaleksic39 <stefanaleksic555@gmail.com>
2025-09-10 14:45:13 +0100

    Dodavanje klase SessionMetadata u Common

---
+++ OfficeSensor/Common/SessionMetadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class SessionMetadata
+    {
+        [DataMember]
+        public double Volume { get; set; }
+
+        [DataMember]
+        public double RelativeHumidity { get; set; }
+
+        [DataMember]
+        public double AirQuality { get; set; }
+
+        [DataMember]
+        public double LightLevel { get; set; }
+
+        [DataMember]
+        public DateTime DateTime { get; set; }
+    }
+}
