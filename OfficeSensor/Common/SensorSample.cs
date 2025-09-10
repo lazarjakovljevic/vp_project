232a2a288c4d2bc7a7c2dd71deff9953dc110b43
Lazar <Lazar@LAZAR>
2025-09-10 15:42:06 +0200

    Dodavanje klase SensorSample i njenih polja

---
+++ OfficeSensor/Common/SensorSample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class SensorSample
+    {
+        [DataMember]
+        public double Volume { get; set; }
+
+        [DataMember]
+        public double RelativeHumidity { get; set; }
+
+        [DataMember]
+        public double AirQuality { get; set; }
+
+        [DataMember]
+        public double LightLevel { get; set; }
+
+        [DataMember]
+        public DateTime DateTime { get; set; }
+    }
+}
