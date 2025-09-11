1c83ae2d377fa6b2b829e8a12a311545d94c35a7
Lazar <Lazar@LAZAR>
2025-09-11 14:07:05 +0200

    Dodavanje klasa DataFormatFault i ValidationFault radi standardizovanih izuzetaka

---
+++ OfficeSensor/Common/DataFormatFault.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class DataFormatFault
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string Details { get; set; }
+
+        [DataMember]
+        public string FieldName { get; set; }
+    }
+}
---
+++ OfficeSensor/Common/ValidationFault.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class ValidationFault
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string FieldName { get; set; }
+
+        [DataMember]
+        public object InvalidValue { get; set; }
+
+        [DataMember]
+        public string ExpectedRange { get; set; }
+    }
+}
